8e341073ee1e29ba9052e20abda9e7386508f15d
loukaspd <loukaspd@gmail.com>
2020-03-14 17:56:55 +0200

    [event-entry] adds expand - collapse toggle

--- src/app/components/event-viewer/event-entry-item/event-entry-item.component.ts
+++ src/app/components/event-viewer/event-entry-item/event-entry-item.component.ts
@@ -1,6 +1,7 @@
 //#region imports
 import { Component, Input } from '@angular/core';
 import {Event} from '../../../types/Event';
+import { GlobalUtils } from '../../../services/global-utils';
 //#endregion imports
 
 @Component({
@@ -10,8 +11,45 @@ import {Event} from '../../../types/Event';
 })
 export class EventEntryItemComponent {
     //#region Constructor & Properties
-    @Input() public event: Event;
+    private _event: Event;
+    @Input()
+    set event(value :Event) {
+        this._onInputChanged(value);
+    }
+    get event(): Event {return this._event;}
+
+    public uiHasBigContent: boolean;
+    public uiShowingAllContent: boolean;
+    public uiEventMessage: String;
+    
     //#endregion Constructor & Properties
 
+    //#region Ui Methods
+    public uiOnToggleClicked() :void {
+        this.uiShowingAllContent = !this.uiShowingAllContent;
+        this.uiEventMessage = this.uiShowingAllContent 
+            ? this._event.Message
+            : this._messageSmallPart();
+    }
+    //#endregion
 
+    //#region Implementation
+    private _onInputChanged(event: Event): void {
+        this._event = event;
+        this.uiShowingAllContent = false;
+        
+        const messageLines = GlobalUtils.splitLines(this._event.Message);
+        this.uiHasBigContent = messageLines.length > 2; //TODO
+        
+        this.uiEventMessage = !this.uiHasBigContent ? this._event.Message
+        : this._messageSmallPart();
+    }
+
+    //TODO
+    private _messageSmallPart(): string {
+        const messageLines = GlobalUtils.splitLines(this._event.Message);
+
+        return [messageLines[0],messageLines[1]].join('\n');
+    }
+    //#endregion Implementation
 }
