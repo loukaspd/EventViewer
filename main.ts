f5e75976a41ee2ba38367a19a0289dae600087d5
loukaspd <loukaspd@gmail.com>
2020-07-17 21:14:44 +0300

    cleanup autoupdated loging

--- main.ts
+++ main.ts
@@ -64,7 +64,7 @@ try {
   // Some APIs can only be used after this event occurs.
   app.on('ready', () => {
     createWindow();
-    setupAutoUpdater();
+    //setupAutoUpdaterLogging();
     autoUpdater.checkForUpdatesAndNotify();
   });
 
@@ -91,32 +91,10 @@ try {
 }
 
 //#region autoUpdater
-function setupAutoUpdater() {
+function setupAutoUpdaterLogging() {
+  //%USERPROFILE%\AppData\Roaming\event-viewer-pp\logs\main.log
   const log = require("electron-log");
   log.transports.file.level = "debug";
-  log.info('App starting...');
   autoUpdater.logger = log;
-
-  autoUpdater.on('checking-for-update', () => {
-    console.log('Checking for update...');
-  })
-  autoUpdater.on('update-available', (info) => {
-    console.log('Update available.');
-  })
-  autoUpdater.on('update-not-available', (info) => {
-    console.log('Update not available.');
-  })
-  autoUpdater.on('error', (err) => {
-    console.log('Error in auto-updater. ' + err);
-  })
-  autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    console.log(log_message);
-  })
-  autoUpdater.on('update-downloaded', (info) => {
-    console.log('Update downloaded');
-  });
 }
 //#endregion
